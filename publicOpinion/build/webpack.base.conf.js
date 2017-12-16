398fa22963ce9fe0344065aff627e638ab3f0c32
huhao <hhn4080@corp.netease.com>
2017-12-16 11:17:31 +0800

    eslint auto fix

--- publicOpinion/build/webpack.base.conf.js
+++ publicOpinion/build/webpack.base.conf.js
@@ -15,7 +15,9 @@ const createLintingRule = () => ({
   include: [resolve('src'), resolve('test')],
   options: {
     formatter: require('eslint-friendly-formatter'),
-    emitWarning: !config.dev.showEslintErrorsInOverlay
+    emitWarning: !config.dev.showEslintErrorsInOverlay,
+    //autofix 
+    fix: true
   }
 })
 
